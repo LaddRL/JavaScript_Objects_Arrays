e9ff7f4e7de385817fd4e8f3645cf15376935398
ladd lindsay <laddrlindsay@gmail.com>
2023-03-16 15:04:30 -0700

    Finished exrcises 1 and 2!

--- app.js
+++ app.js
@@ -1,9 +1,26 @@
-console.log("Hello World!\n==========\n");
+
 
 // Exercise 1 Section
-console.log("EXERCISE 1:\n==========\n");
+// const numbers = [2,22,12,17,18,39,129];
+// let sum = 0;
+// function arraySum(sum) {
+//     for (let i = 0; i < numbers.length; i++) {
+//         sum += numbers[i]
+//         console.log(sum)
+//     }
+//     return sum;
+// }
+// arraySum(sum);
 
 // Exercise 2 Section
-console.log("EXERCISE 2:\n==========\n");
-
+const book = {
+    title: "Berserk",
+    author: "Kentaro Miura",
+    pages: 100,
+    readCount: 1,
+    info: function() {
+        return this.title +" by " + this.author +", " + this.pages +" pages, read " + this.readCount +" time."
+    }
+}
+console.log(book.info())
 
