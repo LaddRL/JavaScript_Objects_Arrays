a0e3c1dbf7df92deb34d2e57bbc8eae5d459b6ad
ladd lindsay <laddrlindsay@gmail.com>
2023-03-17 14:27:15 -0700

    finished the exercises

--- app.js
+++ app.js
@@ -25,22 +25,46 @@
 // console.log(book.info())
 
 // Exercise 3 Section
-let sentence = "The quick brown fox jumps over the lazy dog";
+// let sentence = "The quick brown fox jumps over the lazy dog";
 
-function reverseWord(sentence) {
-    let result = [];
-    let words = sentence.split(" ");
-    console.log("these are the words split", words);
-    for (let i = 0;i < words.length ; i++) {
-        let letters = words[i].split('');
-        console.log("here are the letters split", letters);
-        for(let j = letters.length - 1; j >= 0; j--){
-            result.push(letters[j]);
-            console.log("this is after the push for result", result);
-        }
-        result.push(" ")
-    }
-    return result.join('');
+// function reverseWord(sentence) {
+//     let result = [];
+//     let words = sentence.split(" ");
+//     console.log("these are the words split", words);
+//     for (let i = 0;i < words.length ; i++) {
+//         let letters = words[i].split('');
+//         console.log("here are the letters split", letters);
+//         for(let j = letters.length - 1; j >= 0; j--){
+//             result.push(letters[j]);
+//             console.log("this is after the push for result", result);
+//         }
+//         result.push(" ")
+//     }
+//     return result.join('');
+// }
+
+// console.log(reverseWord(sentence));
+
+// Exercise 4 Section
+let csvData = "name,age\nFrodo,50\nSam,38\nMerry,36\nPippin,26";
+
+const head = csvData.split('\n')[0].split(',')[0]
+const head1 = csvData.split('\n')[0].split(',')[1]
+
+const data = csvData.split('\n').slice(1)
+myArr = []
+
+const myObj = {
+    [head]: "",
+    [head1]: ""
 }
 
-console.log(reverseWord(sentence));
+data.forEach((val)=>{
+    let dataVal = val.split(",");
+    let dataObj = Object.create(myObj);
+    dataObj[head] = dataVal[0];
+    dataObj[head1] = dataVal[1];
+    myArr.push(dataObj);
+})
+
+console.log(myArr);
